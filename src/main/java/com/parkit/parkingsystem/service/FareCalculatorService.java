4993a030e6b41c94a5190e68d869d8ddf01e8897
Eric Alterio <erictalterio@gmail.com>
2023-06-08 14:49:58 +0200

    Initial commit - Added 30min free

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,29 +3,45 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+            throw new IllegalArgumentException("Out time provided is incorrect: " + ticket.getOutTime());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date inTime = dateFormat.parse(dateFormat.format(ticket.getInTime()));
+            Date outTime = dateFormat.parse(dateFormat.format(ticket.getOutTime()));
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+            long durationMillis = outTime.getTime() - inTime.getTime();
+            int durationMinutes = (int) TimeUnit.MILLISECONDS.toMinutes(durationMillis);
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
+            if (durationMinutes <= 30) {
+                ticket.setPrice(0); // Set fare to 0 if duration is less than or equal to 30 minutes
+            } else {
+                long durationHours = TimeUnit.MILLISECONDS.toHours(durationMillis);
+
+            switch (ticket.getParkingSpot().getParkingType()){
+                case CAR: {
+                    ticket.setPrice(durationHours * Fare.CAR_RATE_PER_HOUR);
+                    break;
+                }
+                case BIKE: {
+                    ticket.setPrice(durationHours * Fare.BIKE_RATE_PER_HOUR);
+                    break;
+                }
+                default: throw new IllegalArgumentException("Unknown Parking Type");
+                }
             }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Error parsing date: " + e.getMessage());
         }
     }
 }
